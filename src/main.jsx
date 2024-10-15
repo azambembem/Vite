3f64b23a13406cc6e055fc91f0ad75f6d02ce7b4
azambembem <azamjan980713@gmail.com>
2024-10-15 16:13:44 +0900

    all pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,17 @@
-// HashRouter -> Ba'zi sabablarga ko'ra URL manzillarini serverga yubora olmaydigan yoki yubora olmaydigan veb-brauzerlarda foydalanish uchun mo'ljallangan.
-// zarur bulmasa ishlatmaslik yaxshi
-
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import App from "./App";
+import store from "./redux1/store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-const Home = () => <h2>Home Page</h2>;
-
-const About = () => <h2>About Page</h2>;
-
-const App = () => {
-  return (
-    // HashRouter initialEntries={["/"]} ->  nima activeligi ko'rsatish uchun
-    <HashRouter initialEntries={["/"]}>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </HashRouter>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
